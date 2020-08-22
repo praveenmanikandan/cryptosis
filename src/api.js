e8aa7cf7bb034a5513893527768a6585306be4d6
praveenmanikandan <praveenmanikandan2000@gmail.com>
2020-08-21 23:19:01 -0700

    netlify deploying

---
+++ src/api.js
@@ -0,0 +1,16 @@
+const express=require("express")
+const serverless=require("serverless-http")
+
+
+const app=express()
+
+const router=express.Router();
+router.get('/',(req,res)=>{
+    res.json({
+        message:"Hi!"
+    })
+})
+
+app.use('/.netlify/functions/api',router)
+
+module.exports.handler=serverless(app)
